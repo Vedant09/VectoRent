44d438b2aeb0ae0e2526bed1a61eee32fd8f9124
Vedant09 <vedantvy98@gmail.com>
2024-05-15 02:39:28 -0400

    added git ignore

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import phone from '../images/phone.png'
-import mail from '../images/Mail.png'
+import React from "react";
+import phone from "../images/phone.png";
+import mail from "../images/Mail.png";
 
-export default function Contact(props){
+export default function Contact(props) {
   // console.log(props.contacts.status)
-  let badgeName
-  if(props.status === 0){
-    badgeName = "CEO"
-  }else if(props.status === 1){
-    badgeName = "Developer"
-  }else if(props.status === 2){
-    badgeName = "Tester"
+  let badgeName;
+  if (props.status === 0) {
+    badgeName = "CEO";
+  } else if (props.status === 1) {
+    badgeName = "Developer";
+  } else if (props.status === 2) {
+    badgeName = "Tester";
   }
 
-
   // let count = 0
-  const [count,setCount] = React.useState(0)
-  function ClicOver(){
-    setCount(count+1);
+  const [count, setCount] = React.useState(0);
+  function ClickOver() {
+    setCount(count + 1);
     // console.log(`count click on ${props.name} is ${count+1}`);
     // return count;
   }
-  return( props.id &&
-    <div onClick={ClicOver} className="contact-card">
-      {badgeName && <div className='card-badge'>{badgeName}</div>}
-            <img src={props.img} alt=""/>
-            <h3>{props.name}</h3>
-            <div className="info-group">
-                <img src={phone} alt=''/>
-                <p>{props.phone}</p>
-            </div>
-            <div className="info-group">
-                <img src={mail} alt=''/>
-                <p>{props.email}</p>
-            </div>
-            <p>{`Endorsed by ${count} Leaders`}</p>
-    </div>
-  )
+  return (
+    props.id && (
+      <div onClick={ClickOver} className="contact-card">
+        {badgeName && <div className="card-badge">{badgeName}</div>}
+        <img src={props.img} alt="" />
+        <h3>{props.name}</h3>
+        <div className="info-group">
+          <img src={phone} alt="" />
+          <p>{props.phone}</p>
+        </div>
+        <div className="info-group">
+          <img src={mail} alt="" />
+          <p>{props.email}</p>
+        </div>
+        <p>{`Endorsed by ${count} Leaders`}</p>
+      </div>
+    )
+  );
 }
